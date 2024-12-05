288aeef492ef248665b4f8756e35e9577006b9d6
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-06 02:29:33 +0600

    User Get Operation for User Completed

--- index.js
+++ index.js
@@ -62,10 +62,28 @@ async function run() {
 
 
     // User Related API
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.post("/users", async (req, res) => {
       const newUser = req.body;
-      console.log("Creating New User", user);
       const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    })
+
+    app.patch("/users", async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+        }
+      }
+
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
 
